3b076bea446831584bd8a4f0fb5f975b40cc2d59
Nguyen Lam <slamnguyengp@gmail.com>
2023-09-18 22:39:02 +0700

    hello

---
+++ demolambda/src/main/java/com/demolambda/DemolambdaApplication.java
@@ -0,0 +1,13 @@
+package com.demolambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemolambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemolambdaApplication.class, args);
+	}
+
+}
---
+++ demolambda/src/test/java/com/demolambda/DemolambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demolambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemolambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
